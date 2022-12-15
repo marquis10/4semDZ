9cbe9d3290538e50154d7b9ebb4465e9747ef005
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-15 22:00:12 +0500

    25z

---
+++ 25z/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine()), res = A;
+for (int i = 1;  i < B; i++)
+{
+    res = res * A;
+}    
+Console.WriteLine($"Число A в степени B = {res}");
---
+++ 27z/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), res = 0, temp = 0;
+while (n > 0)
+    {
+        temp = n % 10;
+        n /= n;
+        res = res + temp;
+    }
+Console.WriteLine($"Сумма цифр в числе {n} = {res}");
