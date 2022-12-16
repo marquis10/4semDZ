1aa0119e86b9b1248d3135bbc703b83374e80a4f
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-16 12:50:01 +0500

    29z

--- 29z/Program.cs
+++ 29z/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine()), j = 0;
+int [] array = new int [n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Массив array: [{string.Join(", ", array)}]");
