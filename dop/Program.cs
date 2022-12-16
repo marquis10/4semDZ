de6871da7bd7cfae021bbbcdb92b5cdd009eae80
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-16 13:03:35 +0500

    dop

---
+++ dop/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine()), j = 0;
+int [] array = new int [n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Массив array: [{string.Join(", ", array)}]");
